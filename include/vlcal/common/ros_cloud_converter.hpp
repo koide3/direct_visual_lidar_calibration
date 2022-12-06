98746adae5d7b3e54a11bab3c0dcb5911e7fe57c
k.koide <k.koide@aist.go.jp>
2022-12-05 17:11:07 -0800

    ROS1

---
+++ include/vlcal/common/ros_cloud_converter.hpp
@@ -0,0 +1,181 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <iostream>
+#include <boost/format.hpp>
+
+#include <Eigen/Core>
+#include <vlcal/common/raw_points.hpp>
+
+#ifdef VLCAL_ROS2
+#include <sensor_msgs/msg/point_cloud2.hpp>
+namespace vlcal {
+using PointCloud2 = sensor_msgs::msg::PointCloud2;
+using PointCloud2Ptr = sensor_msgs::msg::PointCloud2::SharedPtr;
+using PointCloud2ConstPtr = sensor_msgs::msg::PointCloud2::ConstSharedPtr;
+using PointField = sensor_msgs::msg::PointField;
+
+template <typename Stamp>
+double to_sec(const Stamp& stamp) {
+  return stamp.sec + stamp.nanosec / 1e9;
+}
+
+inline builtin_interfaces::msg::Time from_sec(const double time) {
+  builtin_interfaces::msg::Time stamp;
+  stamp.sec = std::floor(time);
+  stamp.nanosec = (time - stamp.sec) * 1e9;
+  return stamp;
+}
+
+}  // namespace vlcal
+#else
+#include <sensor_msgs/PointCloud2.h>
+namespace vlcal {
+using PointCloud2 = sensor_msgs::PointCloud2;
+using PointCloud2Ptr = sensor_msgs::PointCloud2::Ptr;
+using PointCloud2ConstPtr = sensor_msgs::PointCloud2::ConstPtr;
+using PointField = sensor_msgs::PointField;
+
+template <typename Stamp>
+double to_sec(const Stamp& stamp) {
+  return stamp.toSec();
+}
+
+inline ros::Time from_sec(const double time) {
+  ros::Time stamp;
+  stamp.sec = std::floor(time);
+  stamp.nsec = (time - stamp.sec) * 1e9;
+  return stamp;
+}
+
+}  // namespace glim
+#endif
+
+namespace vlcal {
+
+template <typename T>
+Eigen::Vector4d get_vec4(const void* x, const void* y, const void* z) {
+  return Eigen::Vector4d(*reinterpret_cast<const T*>(x), *reinterpret_cast<const T*>(y), *reinterpret_cast<const T*>(z), 1.0);
+}
+
+static RawPoints::Ptr extract_raw_points(const PointCloud2& points_msg, const std::string& intensity_channel = "intensity") {
+  int num_points = points_msg.width * points_msg.height;
+
+  int x_type = 0;
+  int y_type = 0;
+  int z_type = 0;
+  int time_type = 0;  // ouster and livox
+  int intensity_type = 0;
+
+  int x_offset = -1;
+  int y_offset = -1;
+  int z_offset = -1;
+  int time_offset = -1;
+  int intensity_offset = -1;
+
+  std::unordered_map<std::string, std::pair<int*, int*>> fields;
+  fields["x"] = std::make_pair(&x_type, &x_offset);
+  fields["y"] = std::make_pair(&y_type, &y_offset);
+  fields["z"] = std::make_pair(&z_type, &z_offset);
+  fields["t"] = std::make_pair(&time_type, &time_offset);
+  fields["time"] = std::make_pair(&time_type, &time_offset);
+  fields["time_stamp"] = std::make_pair(&time_type, &time_offset);
+  fields["timestamp"] = std::make_pair(&time_type, &time_offset);
+  fields[intensity_channel] = std::make_pair(&intensity_type, &intensity_offset);
+
+  for (const auto& field : points_msg.fields) {
+    auto found = fields.find(field.name);
+    if (found == fields.end()) {
+      continue;
+    }
+
+    *found->second.first = field.datatype;
+    *found->second.second = field.offset;
+  }
+
+  if (x_offset < 0 || y_offset < 0 || z_offset < 0) {
+    std::cerr << "waning: missing point coordinate fields" << std::endl;
+    return nullptr;
+  }
+
+  if ((x_type != PointField::FLOAT32 && x_type != PointField::FLOAT64) || x_type != y_type || x_type != y_type) {
+    std::cerr << "warning: unsupported points type" << std::endl;
+    return nullptr;
+  }
+
+  std::vector<Eigen::Vector4d, Eigen::aligned_allocator<Eigen::Vector4d>> points;
+  points.resize(num_points);
+  for (int i = 0; i < num_points; i++) {
+    const auto* x_ptr = &points_msg.data[points_msg.point_step * i + x_offset];
+    const auto* y_ptr = &points_msg.data[points_msg.point_step * i + y_offset];
+    const auto* z_ptr = &points_msg.data[points_msg.point_step * i + z_offset];
+
+    if (x_type == PointField::FLOAT32) {
+      points[i] = get_vec4<float>(x_ptr, y_ptr, z_ptr);
+    } else {
+      points[i] = get_vec4<double>(x_ptr, y_ptr, z_ptr);
+    }
+  }
+
+  std::vector<double> times;
+  if (time_offset >= 0) {
+    times.resize(num_points);
+
+    for (int i = 0; i < num_points; i++) {
+      const auto* time_ptr = &points_msg.data[points_msg.point_step * i + time_offset];
+      switch (time_type) {
+        case PointField::UINT32:
+          times[i] = *reinterpret_cast<const uint32_t*>(time_ptr) / 1e9;
+          break;
+        case PointField::FLOAT32:
+          times[i] = *reinterpret_cast<const float*>(time_ptr);
+          break;
+        case PointField::FLOAT64:
+          times[i] = *reinterpret_cast<const double*>(time_ptr);
+          break;
+        default:
+          std::cerr << "warning: unsupported time type " << time_type << std::endl;
+          return nullptr;
+      }
+    }
+  }
+
+  std::vector<double> intensities;
+  if (intensity_offset >= 0) {
+    intensities.resize(num_points);
+
+    for (int i = 0; i < num_points; i++) {
+      const auto* intensity_ptr = &points_msg.data[points_msg.point_step * i + intensity_offset];
+      switch (intensity_type) {
+        case PointField::UINT8:
+          intensities[i] = *reinterpret_cast<const std::uint8_t*>(intensity_ptr);
+          break;
+        case PointField::UINT16:
+          intensities[i] = *reinterpret_cast<const std::uint16_t*>(intensity_ptr);
+          break;
+        case PointField::UINT32:
+          intensities[i] = *reinterpret_cast<const std::uint32_t*>(intensity_ptr);
+          break;
+        case PointField::FLOAT32:
+          intensities[i] = *reinterpret_cast<const float*>(intensity_ptr);
+          break;
+        case PointField::FLOAT64:
+          intensities[i] = *reinterpret_cast<const double*>(intensity_ptr);
+          break;
+        default:
+          std::cerr << "warning: unsupported intensity type" << std::endl;
+          return nullptr;
+      }
+    }
+  }
+
+  const double stamp = to_sec(points_msg.header.stamp);
+  return RawPoints::Ptr(new RawPoints{stamp, times, intensities, points});
+}
+
+static RawPoints::Ptr extract_raw_points(const PointCloud2ConstPtr& points_msg, const std::string& intensity_channel = "intensity") {
+  return extract_raw_points(*points_msg);
+}
+
+}  // namespace vlcal
---
+++ src/preprocess_ros1.cpp
@@ -0,0 +1,126 @@
+#include <vlcal/preprocess/preprocess.hpp>
+
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#define GLIM_ROS2
+#include <vlcal/common/ros_cloud_converter.hpp>
+
+namespace vlcal {
+
+class PointCloudReaderROS1 : public PointCloudReader {
+public:
+  PointCloudReaderROS1(const std::string& bag_filename, const std::string& points_topic, const std::string& intensity_channel) : intensity_channel(intensity_channel) {
+    reader.open(bag_filename);
+    view = std::make_shared<rosbag::View>(reader, rosbag::TopicQuery(points_topic));
+
+    msg_itr = view->begin();
+  }
+
+  virtual RawPoints::Ptr read_next() override {
+    if (msg_itr == view->end()) {
+      return nullptr;
+    }
+
+    const auto points_msg = msg_itr->instantiate<sensor_msgs::PointCloud2>();
+    if (!points_msg) {
+      return nullptr;
+    }
+
+    return extract_raw_points(points_msg, intensity_channel);
+  }
+
+private:
+  const std::string intensity_channel;
+
+  rosbag::Bag reader;
+  std::shared_ptr<rosbag::View> view;
+
+  rosbag::View::iterator msg_itr;
+};
+
+class PreprocessROS1 : public Preprocess {
+protected:
+  template <typename T>
+  boost::shared_ptr<T> get_first_message(const std::string& bag_filename, const std::string& topic) const {
+    rosbag::Bag bag(bag_filename);
+    rosbag::View view(bag, rosbag::TopicQuery(topic));
+
+    for (const auto m : view) {
+      const auto msg = m.instantiate<T>();
+      if (msg) {
+        return msg;
+      }
+    }
+
+    std::cerr << "error: bag does not contain topic " << topic << std::endl;
+    abort();
+    return nullptr;
+  }
+
+  virtual bool valid_bag(const std::string& bag_filename) override {
+    rosbag::Bag bag(bag_filename);
+    return bag.isOpen();
+  }
+
+  virtual std::vector<std::pair<std::string, std::string>> get_topics_and_types(const std::string& bag_filename) override {
+    rosbag::Bag bag(bag_filename);
+    rosbag::View view(bag);
+
+    std::vector<std::pair<std::string, std::string>> topics_and_types;
+    for (const auto& conn : view.getConnections()) {
+      topics_and_types.emplace_back(conn->topic, conn->datatype);
+    }
+
+    return topics_and_types;
+  }
+
+  virtual std::vector<std::string> get_point_fields(const std::string& bag_filename, const std::string& points_topic) override {
+    const auto msg = get_first_message<sensor_msgs::PointCloud2>(bag_filename, points_topic);
+    std::vector<std::string> fields(msg->fields.size());
+    std::transform(msg->fields.begin(), msg->fields.end(), fields.begin(), [](const auto& field) { return field.name; });
+    return fields;
+  }
+
+  virtual cv::Size get_image_size(const std::string& bag_filename, const std::string& image_topic) override {
+    const auto image_msg = get_first_message<sensor_msgs::Image>(bag_filename, image_topic);
+    return cv::Size(image_msg->width, image_msg->height);
+  }
+
+  virtual std::tuple<std::string, std::vector<double>, std::vector<double>> get_camera_info(const std::string& bag_filename, const std::string& camera_info_topic) override {
+    const auto camera_info_msg = get_first_message<sensor_msgs::CameraInfo>(bag_filename, camera_info_topic);
+    std::vector<double> intrinsic(4);
+    intrinsic[0] = camera_info_msg->K[0];
+    intrinsic[1] = camera_info_msg->K[4];
+    intrinsic[2] = camera_info_msg->K[2];
+    intrinsic[3] = camera_info_msg->K[5];
+    std::vector<double> distortion_coeffs = camera_info_msg->D;
+
+    return {camera_info_msg->distortion_model, intrinsic, distortion_coeffs};
+  }
+
+  virtual cv::Mat get_image(const std::string& bag_filename, const std::string& image_topic) override {
+    const auto image_msg = get_first_message<sensor_msgs::Image>(bag_filename, image_topic);
+    return cv_bridge::toCvCopy(*image_msg, "mono8")->image;
+  }
+
+  virtual std::shared_ptr<PointCloudReader> get_point_cloud_reader(const std::string& bag_filename, const std::string& points_topic, const std::string& intensity_channel)
+    override {
+    return std::make_shared<PointCloudReaderROS1>(bag_filename, points_topic, intensity_channel);
+  }
+};
+
+}  // namespace vlcal
+
+int main(int argc, char** argv) {
+  vlcal::PreprocessROS1 preprocess;
+  preprocess.run(argc, argv);
+
+  return 0;
+}
